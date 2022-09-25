9ab165000334fff612771efe421c9f652168d123
veta41 <74262484+veta41@users.noreply.github.com>
2022-09-25 23:38:19 +0300

    fix

--- js/bg-elements.js
+++ js/bg-elements.js
@@ -1,11 +1,9 @@
 const bgElements = () => {
   const bgEl = document.querySelectorAll(".set-bg");
 
-  for (let index = 0; index < bgEl.length; index++) {
-    const src = bgEl[index].dataset.setbg;
-
-    bgEl[index].style.backgroundImage = `url(${src})`;
-  }
+  bgEl.forEach((element) => {
+    element.style.backgroundImage = `url(${element.dataset.setbg})`;
+  });
 };
 
 bgElements();
--- js/mainData.js
+++ js/mainData.js
@@ -1,10 +1,45 @@
 const mainData = () => {
+  const renderAnimeList = (array, genres) => {
+    console.log(array);
+    console.log(genres);
+  };
+
+  const renderTopAnime = (array) => {
+    const wrapper = document.querySelector(".filter__gallery");
+    wrapper.innerHTML = "";
+
+    array.forEach((item) => {
+      wrapper.insertAdjacentHTML(
+        "afterbegin",
+        `
+			<div class="product__sidebar__view__item set-bg" data-setbg="${item.image}">
+				<div class="ep">${item.rating} / 10</div>
+				<div class="view"><i class="fa fa-eye"></i> ${item.views}</div>
+				<h5><a href="/anime-details.html">${item.title}</a></h5>
+	  		</div>
+			`
+      );
+    });
+
+    wrapper.querySelectorAll(".set-bg").forEach((element) => {
+      element.style.backgroundImage = `url(${element.dataset.setbg})`;
+    });
+  };
+
   fetch("./db.json")
     .then((responce) => {
       return responce.json();
     })
     .then((data) => {
-      console.log(data.anime);
+      const ganres = new Set();
+
+      renderTopAnime(data.anime.sort((a, b) => b.views - a.views).slice(0, 5));
+
+      data.anime.forEach((item) => {
+        GeolocationCoordinates.add(item.ganre);
+      });
+
+      renderAnimeList(data.anime, ganres);
     });
 };
 mainData();
