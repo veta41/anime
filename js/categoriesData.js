5a69b67781fa9e91667b5e0777c589660a6b3970
veta41 <74262484+veta41@users.noreply.github.com>
2022-09-26 22:40:04 +0300

    agg categories and detali

--- js/categoriesData.js
+++ js/categoriesData.js
@@ -1,6 +1,5 @@
-const mainData = () => {
+const categoriesData = () => {
   const preloader = document.querySelector(".preloder");
-
   const renderGanreList = (ganres) => {
     const dropdownBlock = document.querySelector(".header__menu .dropdown");
 
@@ -15,15 +14,33 @@ const mainData = () => {
   };
 
   const renderAnimeList = (array, ganres) => {
-    const wrapper = document.querySelector(".product .col-lg-8");
+    const wrapper = document.querySelector(".product-page .col-lg-8");
 
     ganres.forEach((ganre) => {
       const productBlock = document.createElement("div");
       const listBlock = document.createElement("div");
-      const list = array.filter((item) => item.ganre === ganre);
+      const list = array.filter((item) => item.tags.includes(ganre));
 
       listBlock.classList.add("row");
       productBlock.classList.add("mb-5");
+
+      productBlock.insertAdjacentHTML(
+        `beforeend`,
+        `
+          <div class="row">
+              <div class="col-lg-8 col-md-8 col-sm-8">
+                  <div class="section-title">
+                      <h4>${ganre}</h4>
+                  </div>
+              </div>
+              <div class="col-lg-4 col-md-4 col-sm-4">
+                  <div class="btn__all">
+                      <a href="./categories.html?ganre=${ganre}" class="primary-btn">View All <span class="arrow_right"></span></a>
+                  </div>
+              </div>
+          </div>
+      `
+      );
 
       list.forEach((item) => {
         const tagsBlock = document.createElement("ul");
@@ -62,25 +79,11 @@ const mainData = () => {
       wrapper.querySelectorAll(".set-bg").forEach((elem) => {
         elem.style.backgroundImage = `url(${elem.dataset.setbg})`;
       });
-
-      productBlock.insertAdjacentHTML(
-        `beforeend`,
-        `
-          <div class="row">
-              <div class="col-lg-8 col-md-8 col-sm-8">
-                  <div class="section-title">
-                      <h4>${ganre}</h4>
-                  </div>
-              </div>
-              <div class="col-lg-4 col-md-4 col-sm-4">
-                  <div class="btn__all">
-                      <a href="./categories.html?ganre=${ganre}" class="primary-btn">View All <span class="arrow_right"></span></a>
-                  </div>
-              </div>
-          </div>
-      `
-      );
     });
+
+    setTimeout(() => {
+      preloader.classList.remove("active");
+    }, 500);
   };
 
   const renderTopAnime = (array) => {
@@ -102,25 +105,28 @@ const mainData = () => {
     wrapper.querySelectorAll(".set-bg").forEach((elem) => {
       elem.style.backgroundImage = `url(${elem.dataset.setbg})`;
     });
-
-    setTimeout(() => {
-      preloader.classList.remove("active");
-    }, 500);
   };
 
   fetch("./db.json")
     .then((response) => response.json())
     .then((data) => {
       const ganres = new Set();
-
+      const ganreParams = new URLSearchParams(window.location.search).get(
+        "ganre"
+      );
+      console.log(ganreParams);
       data.anime.forEach((item) => {
         ganres.add(item.ganre);
       });
 
       renderTopAnime(data.anime.sort((a, b) => b.views - a.views).slice(0, 5));
-      renderAnimeList(data.anime, ganres);
+      if (ganreParams) {
+        renderAnimeList(data.anime, [ganreParams]);
+      } else {
+        renderAnimeList(data.anime, ganres);
+      }
       renderGanreList(ganres);
     });
 };
 
-mainData();
+categoriesData();
